1dca1749a9c88ee2786fb616eed23dc4fb0beb65
rinamakey <karaleva85@mail.ru>
2024-10-18 11:24:25 +0300

    first commit

--- script.js
+++ script.js
@@ -24,8 +24,6 @@ gsap.from('li',{opacity:0,
     duration:1,
     delay:0.5})
 
-gsap.registerPlugin(ScrollTrigger)
-
 
 
 
@@ -35,51 +33,55 @@ gsap.registerPlugin(ScrollTrigger)
 gsap.from('.text-block__h', {opacity: 0,
     duration:4,
     x: 50,
-    delay:0.2,
-    scrub:true
+    delay:0.2
+    // scrub:true
 })
 
 gsap.from('.text-block__p', {opacity: 0,
     x: 50,
     duration: 5,
-    delay: 0.4,
-    start:'center',
-    end:820,
-    scrub:true
+    delay: 0.4
+    // start:'center',
+    // end:820,
+    // scrub:true
     })
+
+
+    gsap.registerPlugin(ScrollTrigger)
+
     gsap.from('.gsr', {opacity: 0,
         scrollTrigger:{
             trigger:"gsr", 
-            toggleActions:"restart restart revers none"
+            toggleActions: "restart none none none"
         },
         x: -50,
         duration: 4,
-        start:'center',
-        end:820,
-        scrub:true
+        start: "20px 80%" ,
+        end: "bottom 100px"
+        // scrub:true
     })
     
 gsap.from('.gs', {opacity: 0,
     scrollTrigger:{
         trigger:"gs", 
-        toggleActions:"restart restart revers none"
+        toggleActions:"restart none none none"
       },
     x: 50,
     duration:4,
     start:'center',
-    end:820,
-    scrub:true
+    end:820
+    // scrub:true
 })
 gsap.from('.gsa', {opacity: 0,
     scrollTrigger:{
         trigger:"gsa", 
-        toggleActions:"restart  revers  restart none"
+        toggleActions:"restart none none none"
       },
     x: 50,
     duration:4,
     start:'center',
-    end:820,
-    scrub:true
+    end:820
+    // scrub:true
 })
 
 
@@ -110,6 +112,8 @@ gsap.from(".an ",{ y: 30,
 gsap.from(".one",{ x: 30,  stagger:0.7, delay: 0.6, duration: 5, opacity: 0 })
 
 gsap.from(".fontsca",{ y: 20,  stagger:0.7, delay: 0.3, duration: 5, opacity: 0 })
+
+
 
 gsap.from(".animals",{ x: -30, 
     scrollTrigger:{
